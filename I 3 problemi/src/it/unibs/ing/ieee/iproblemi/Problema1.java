66d0dbaff80b6e1d2590f77bfe9ff75aedd89bda
danielevezzoli <d.vezzoli013@studenti.unibs.it>
2017-06-05 13:58:03 +0200

    Primo problema, inseriti anche i casi 10 e 1000

--- I 3 problemi/src/it/unibs/ing/ieee/iproblemi/Problema1.java
+++ I 3 problemi/src/it/unibs/ing/ieee/iproblemi/Problema1.java
@@ -14,16 +14,35 @@ public class Problema1 {
 	public static void esegui() {
 		LogTime log = new LogTime();
 
-		long below = 100;
+		long below = 10;
 
 		log.getTime();
-
+		
 		long n5 = (long) Math.ceil(below / 5);
 		long n3 = (long) Math.ceil(below / 3);
 
 		long sum = ((n3 * 3 * (1 + n3)) + (n5 * 5 * (1 + n5))) / 2;
 
+		
+		below = 100;
+
+		
+		n5 = (long) Math.ceil(below / 5);
+		n3 = (long) Math.ceil(below / 3);
+
+		sum = ((n3 * 3 * (1 + n3)) + (n5 * 5 * (1 + n5))) / 2;
+
+		
+		below = 1000;
+
+		
+		n5 = (long) Math.ceil(below / 5);
+		n3 = (long) Math.ceil(below / 3);
+
+		sum = ((n3 * 3 * (1 + n3)) + (n5 * 5 * (1 + n5))) / 2;
+
 		log.getTime();
+
 
 		System.out.println(sum);
 
